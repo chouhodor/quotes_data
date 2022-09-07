ffedc3750ca1749798b9d8992b840136099089f2
chouhodor <31790084+chouhodor@users.noreply.github.com>
2022-09-07 09:41:11 +0800

    new

---
+++ quotes.py
@@ -0,0 +1,123 @@
+quotes = ["When price dropped, they dont want to praise the government. But when it rises, they blame the government… This is not fair as it is due to weather conditions. – Najib Razak",
+
+"Malaysia tidak pernah dijajah, sebaliknya, hanya dinaungi atau dilindungi oleh British. – Zainal Kling",
+
+"Had a good chat yesterday with Mark Zuckerberg at Twitter HQ. – Najib Razak",
+
+"Beli sendiri guna duit menabung dari kecil – Rosmah",
+
+"We are all Malaysians. This is the bond that unites us. Let us always remember that unity is our fundamental strength as a people and as a nation. ~ Tunku Abdul Rahman",
+
+"Life in this world is short. Let us make use of our lives in the pursuit of happiness and not trouble. ~ Tunku Abdul Rahman",
+
+
+"All talk on Islamic States is just an empty dream. No man in his right sense would accept a nation which bases its political administration on religion, and in a country like Malaysia with its multiracial and multireligious people, there is no room for an IS -T. Abdul Rahman",
+
+"In the old days people never bothered about what others did, so long as they were free to do what they liked themselves. Today, one cannot sneeze without being corrected, let alone enjoy oneself. That's what politics have done to our society ~ Tunku Abdul Rahman",
+
+
+"Islam is a religion based upon knowledge, and a denial of the possibility and objectivity of knowledge would involve the destruction of the fundamental basis upon which not only the religion, but all the sciences are rooted. ~ Syed Muhammad Naquib al-Attas",
+
+"Justice and injustice indeed begins and ends with the self. ~ Syed Muhammad Naquib al-Attas",
+
+"Justice implies knowledge of the right and proper place for a thing or a being to be; of right as against wrong; of the mean and limit; of spiritual gain as against loss of truth as against falsehood. ~ Syed Muhammad Naquib al-Attas",
+
+"Allah is my witness. I will not be silent. I will never surrender. ― Anwar Ibrahim",
+
+"Free and quality education is a fundamental liberty ― Anwar Ibrahim",
+
+"Tradisi ilmu itu penting. Kita berjuang kerana maklumat dan fakta. Kita berjuang kerana cita-cita murni untuk membawa perubahan dan ini bagi saya tidak boleh ditinggalkan dalam arena perjuangan politik kita. ― Anwar Ibrahim",
+
+"Kenapa kita anjurkan reformasi? Kerana kita percaya bahawa negara ini harus bangun dari sikap lama dan usang ke arah masa depan yang gemilang dan bermaruah. ― Anwar Ibrahim",
+
+"Jurang kemiskinan antara yang kaya dan miskin mesti diberi tumpuan. Jurang mesti dirapatkan demi kemakmuran dan kesejahteraan kita bersama.― Anwar Ibrahim",
+
+"Setiap Butir Beras Yang Kita Makan Adalah Kerana UMNO - Dr Mohammad Nawal Ariffin",
+
+"Saya tak gilakan kuasa hanya kerana hendak dapatkan kuasa tetapi saya perlukan kuasa untuk berkhidmat kepada rakyat dan memastikan kejayaan pembangunan negara. - Najib Razak",
+
+"Subsidi ialah penyakit, sekali anda menikmatinya, sukar untuk menolaknya.- Najib Razak",
+
+"Insya-Allah kita tidak menjadi laksana tikus yang berada dalam kepuk padi atau semut yang mati dalam gula, tidak seperti mereka yang tidak beristiqamah melompat dan melarikan diri daripada Pas sebagai sebuah parti Islam. - Abdul Hadi Awang",
+
+"Kalau kita sabar sesama kita, kalau zalim pun  hanya jadi penggembala lembu, kalau orang lain memerintah jadi penggembala babi. Nak lawan sangat, nak jadi penggembala babi atau penggembala lembu? Mesti faham, ini politik. - Abdul Hadi Awang",
+
+"Dalam sekawah gulai daging yang dimasak penuh dengan daging yang suci dan diletakkan seketul daging babi didalamnya itulah perumpamaan kerjasama UMNO dan MCA - Abdul Hadi Awang",
+
+"Orang boleh kata kita macam anjing salak bukit sahaja...ya macam anjing salak bukit.(Hakikatnya) anjing tak salak bukit, tapi anjing menyalak babi yang ada di bukit itu.- Abdul Hadi Awang",
+
+"Harga barang naik ialah ketentuan Allah SWT – Fathul Bari",
+
+"Dont believe Najib means don’t believe in Allah – Ismail Sabri",
+
+"Kalau harga ayam mahal jangan makan ayam. Makan ikan dan daging pula. – Hasan Malek, bekas Menteri Perdagangan Dalam Negeri, Koperasi dan Kepenggunaan",
+
+"Rakyat sokong kenaikan harga barang kerana pihak kami tidak menerima laporan – Hasan Malek",
+
+"Pelajar universiti masak sendiri dari makan di luar untuk elak daripada GST – Ahmad Maslan",
+
+"Solat berjemaah sebelum balik sekolah benda bodoh, menyusahkan pelajar – Zaid Ibrahim",
+
+"Orang Malaysia hidap diabetes dan obesiti tanda negara makmur – Bung Mokhtar",
+
+"Kangkung dulu naik, sekarang ini dah turun. Bila turun apa pasal tak puji kerajaan? Bila naik salahkan kerajaan. Ini tidak adil sebab ini adalah keadaan cuaca... - Najib Razak",
+
+"You help me, I help you - Najib Razak",
+
+"Saya ingin menegaskan bahawa kemelut air di Selangor lebih kritikal (daripada 1MDB). - Najib Razak",
+
+"Bagi saya, demokrasi yang lebih baik adalah demokrasi di mana perempuan tidak hanya memiliki hak untuk memilih dan memilih tetapi untuk dipilih. - Michelle Bachelet",
+
+"Jika Anda hanya ingin disukai, Anda akan siap untuk berkompromi pada apa saja kapan saja, dan Anda tidak akan mencapai apa pun. - Margaret Thatcher",
+
+"Hak asasi manusia adalah hak perempuan dan hak perempuan adalah hak asasi manusia. - Hillary Rodham Clinton",
+
+"Ukuran impian Anda harus selalu melebihi kapasitas Anda saat ini untuk mencapainya. Jika impian Anda tidak membuat Anda takut, itu tidak cukup besar. - Ellen Johnson Sirleaf",
+
+"Politik diciptakan dan dimanifestasikan berdasarkan filosofi dan tujuan untuk menyediakan kebahagiaan dan kesejahteraan bagi manusia, tapi yang terjadi adalah sama sekali kebalikannya. - Emha Ainun Najib",
+
+"Ambisi politik tentu wajar saja, selama pandai menginsyafi batasan etika. - Najwa Shihab",
+
+"Bagiku sendiri politik adalah barang yang paling kotor. Lumpur-lumpur yang kotor. Tapi suatu saat di mana kita tidak dapat menghindari diri lagi, maka terjunlah.- Soe Hok Gie",
+
+"Politik adalah cara merampok dunia. Politik adalah cara menggulingkan kekuasaan, untuk menikmati giliran berkuasa. - W.S. Rendra",
+
+"Pendidikan politik rakyat hanya akan berhasil dalam sistem yang demokratis dan adanya jaminan atas HAM. - Munir",
+
+"Manusia pada dasarnya adalah binatang politik. - Aristotle",
+
+"Bahasa politik dirancang untuk membuat kebohongan terdengar jujur dan pembunuhan menjadi dihormati. - George Orwell",
+
+"Seseorang dapat memimpin suatu negara hanya dengan membantunya melihat pandangan yang cerah. Seorang pemimpin adalah penyalur dalam harapan. - Napoléon Bonaparte",
+
+"Politisi juga tidak punya waktu luang, karena mereka selalu mengincar sesuatu di luar kehidupan politik itu sendiri, kekuasaan dan kemuliaan, atau kebahagiaan. - Aristotle.",
+
+"Politik adalah perang tanpa pertumpahan darah, sedangkan perang adalah politik dengan pertumpahan darah. - Mao Zedong",
+
+"Jual candu ada lesen, jual ganja ada lesen, jual belacan tak ada lesen. Saya smuggle dari Malaya. - P. Ramlee",
+
+"Melawan Malayan Union itu perkara kecil, dan yang berat ialah melawan Melayu sendiri. Melawan Melayu itu ialah melawan perangai, tabiat, dan kelakuan Melayu sendiri. - Dato Onn Jaafar",
+
+"Sungguhpun ada diantara kita yang berlainan fikiran dan fahaman serta berlainan taraf dan kedudukan kita, tetapi hakikat tujuan dan maksud kita hanya satu jua, iaitu kita hendak meninggikan dan memilihara maruah serta kesopanan bangsa kita. - Dato Onn Jaafar",
+
+"Seseorang pemimpin mestilah jujur. Kejujuran yang saya maksudkan, bukan sahaja dari segi sokongan yang diberikan kepada perjuangan parti, tetapi juga dalam pemikiran, pertimbangan dan tindakan. - Tun Hussein Onn",
+
+"Kita memikir apakah sumbangan yang patut kita beri kepada negara dan janganlah kita semata-mata memikirkan apa faedah kita patut dapat daripada kerajaan dan negara. - Tun Abdul Razak",
+
+"Kita harus sedar bahawa kita hidup bebas dan merdeka hanya dengan adanya perpaduan dikalangan rakyat. - Tun Abdul Razak",
+
+"Hai...itu tidak adil! Itu namanya diktator. Kanda, adinda rasa lebih baik kita bahagi-bahagkan harta-harta ini secara demokrasi. - P. Ramlee",
+
+"Sentuhan fizikal yang lembut, yang mendidik dengan penuh kasih sayang, yang tidak menyakitkan tetapi menunjukkan ketegasan kita betapa kita nak dia ubah perangai dia. - Siti Zailah berkenaan tips hubungan suami isteri",
+
+"Jadi dalam pilihan raya boleh tak Allah hantar malaikat bawa IC (kad pengenalan) beratur pergi mengundi? Boleh tak? Senang sangat adik-kakok (tuan-tuan dan puan-puan), - Nor Azariza",
+
+"F**k You - Bung Moktar",
+
+"Ia (RM2 juta itu) tidak (mempunyai nilai terlalu tinggi). Bagi saya ia seperti wang poket sahaja, - Tengku Adnan"
+
+"Tak tahu - Najib Razak, sebanyak 46 kali di mahkamah",
+
+"Cash is King, King is Rakyat - Najib Razak"
+
+]
